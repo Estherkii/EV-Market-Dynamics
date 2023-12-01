da75517ab7adab54e4e23310b5640ad4b8305cdd
Estherkii <123992666+Estherkii@users.noreply.github.com>
2023-12-01 17:39:15 +0100

    upload

--- ev_market.sql
+++ ev_market.sql
@@ -1,6 +1,16 @@
-CREATE DATABASE IF NOT EXISTS ev;
+CREATE DATABASE IF NOT EXISTS ev_market;
+SET SQL_SAFE_UPDATES = 0;
 
-USE ev;
+USE ev_market;
 
--- changing a name column to match tables
+-- delete duplicate row in ev_sales
+DELETE FROM EV_SALES
+WHERE country = "country";
 
+-- delete duplicate row in ev_stock
+DELETE FROM ev_stock
+WHERE country = "country";
+
+
+
+SET SQL_SAFE_UPDATES = 1;
